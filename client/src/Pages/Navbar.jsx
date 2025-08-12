0f6626240e15422e4a73e16a468e9ea284f54552
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-08-12 10:37:42 +0530

    Starting with design

--- client/src/Pages/Navbar.jsx
+++ client/src/Pages/Navbar.jsx
@@ -4,8 +4,8 @@ import "../App.css";
 const Navbar = () => {
   return (
     <div className="p-4   flex justify-between">
-      <i className="fa-solid fa-circle-info text-3xl"></i>
-      <i class="fa-solid fa-gear text-3xl"></i>
+      <i className="fa-solid fa-circle-info text-3xl text-gray-200"></i>
+      <i class="fa-solid fa-gear text-3xl text-gray-200"></i>
     </div>
   );
 };
