d14c19cfe59d6f832fbf2998635ad18c845777ae
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-08-14 23:32:47 +0530

    Developing Transaction Section

--- client/src/Pages/Transactions.jsx
+++ client/src/Pages/Transactions.jsx
@@ -6,7 +6,7 @@ const Transactions = () => {
   return (
     <div
       id="transactions"
-      className="mx-3 rounded-2xl flex flex-col gap-3 mt-10"
+      className="mx-4 rounded-2xl flex flex-col gap-3 mt-10"
     >
       <p className="text-3xl font-semibold">Recent transactions</p>
       <div className="flex ">
